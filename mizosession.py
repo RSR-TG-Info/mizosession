399f06a0b1e05abae528abdd469691d569782bda
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2025-03-29 17:43:09 +0530

    Update mizosession.py

--- mizosession.py
+++ mizosession.py
@@ -1,10 +1,15 @@
+import asyncio
 from pyrogram import Client
 
-API_ID = int(input("Enter API ID: "))
-API_HASH = input("Enter API HASH: ")
+async def main():
+    API_ID = int(input("Enter API ID: "))
+    API_HASH = input("Enter API HASH: ")
+    
+    app = Client(name="test", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+    await app.start()
+    session_string = await app.export_session_string()
+    print(session_string)
+    await app.stop()
 
-app = Client(name="test", api_id=API_ID, api_hash=API_HASH, in_memory=True)
-app.start()
-session_string = app.export_session_string()
-print(session_string)
-app.stop()
+if __name__ == "__main__":
+    asyncio.run(main())
